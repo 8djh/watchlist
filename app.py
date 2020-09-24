2aae4d85e3bac55beb49d9b1342c79b38ec32244
brucewang <402575590@qq.com>
2020-09-24 22:26:09 +0800

    db failed again

--- app.py
+++ app.py
@@ -1,28 +1,18 @@
 import os,sys,click
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
+# SQLite URI compatible
 WIN = sys.platform.startswith('win')
 if WIN: # 如果是 Windows 系统，使用三个斜线
     prefix = 'sqlite:///'
 else: # 否则使用四个斜线
     prefix = 'sqlite:////'
 app=Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('index.html', name=name, movies=movies)
-db=SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
-# 在扩展类实例化前加载配置
-db = SQLAlchemy(app)
-class User(db.Model): # 表名将会是 user（自动生成，小写处理）
-    id = db.Column(db.Integer, primary_key=True) # 主键
-    name = db.Column(db.String(20)) # 名字
-class Movie(db.Model): # 表名将会是 movie
-    id = db.Column(db.Integer, primary_key=True) # 主键
-    title = db.Column(db.String(60)) # 电影标题
-    year = db.Column(db.String(4)) # 电影年份
+db=SQLAlchemy(app)
+
 @app.cli.command() # 注册为命令
 @click.option('--drop', is_flag=True, help='Create after drop.')
 # 设置选项
@@ -32,16 +22,48 @@ def initdb(drop):
         db.drop_all()
     db.create_all()
     click.echo('Initialized database.') # 输出提示信息
-name = 'BruceWang'
-movies = [
-{'title': 'My Neighbor Totoro', 'year': '1988'},
-{'title': 'Dead Poets Society', 'year': '1989'},
-{'title': 'A Perfect World', 'year': '1993'},
-{'title': 'Leon', 'year': '1994'},
-{'title': 'Mahjong', 'year': '1996'},
-{'title': 'Swallowtail Butterfly', 'year': '1996'},
-{'title': 'King of Comedy', 'year': '1999'},
-{'title': 'Devils on the Doorstep', 'year': '1999'},
-{'title': 'WALL-E', 'year': '2008'},
-{'title': 'The Pork of Music', 'year': '2012'},
-]
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    name = 'BruceWang'
+    movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user= User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+class User(db.Model): # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True) # 主键
+    name = db.Column(db.String(20)) # 名字
+
+class Movie(db.Model): # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True) # 主键
+    title = db.Column(db.String(60)) # 电影标题
+    year = db.Column(db.String(4)) # 电影年份
+
+@app.route('/')
+def index():
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
+
+
